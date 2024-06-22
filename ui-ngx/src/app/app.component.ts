ecb516463d6ca9b5aa9bf1b01d41236cd1d504b0
Al khan Abdul Gani <alkhan@lekir.tech>
2024-06-23 07:57:03 +0800

    styling

--- ui-ngx/src/app/app.component.ts
+++ ui-ngx/src/app/app.component.ts
@@ -47,7 +47,7 @@ export class AppComponent implements OnInit {
               private domSanitizer: DomSanitizer,
               private authService: AuthService) {
 
-    console.log(`ThingsBoard Version: ${env.tbVersion}`);
+    console.log(`IotBoard Version: ${env.tbVersion}`);
 
     this.matIconRegistry.addSvgIconResolver((name, namespace) => {
       if (namespace === 'mdi') {
